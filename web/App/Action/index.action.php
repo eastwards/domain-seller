e6d900992e3277c347acd45e39f18456bc634719
eastwards <xtvj007@gmail.com>
2016-06-17 17:10:24 +0800

    first commit

---
+++ web/App/Action/index.action.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * 网站首页
+ *
+ * 网站首页
+ *
+ * @package     Action
+ * @author      void
+ * @since       2014-12-17
+ */
+class IndexAction extends AppAction
+{
+
+    public function index()
+    {
+	$this->set('domain', $_SERVER['HTTP_HOST']);
+        $this->display();
+    }
+
+}
+?>
