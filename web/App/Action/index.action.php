ac4292a7cb6a7098949937395f1c3b121dd41b31
eastwards <xtvj007@gmail.com>
2016-06-22 18:30:58 +0800

    delete Log

--- web/App/Action/index.action.php
+++ web/App/Action/index.action.php
@@ -16,6 +16,15 @@ class IndexAction extends AppAction
 
     public function index()
     {
+	$strDomain = C('ALLOW');
+	$arrDomain = $tmpArr = explode(',', $strDomain);
+	$tmpArr = array_map(function($n){return 'www.'.$n;}, $tmpArr);
+	$arr = array_merge($arrDomain, $tmpArr);
+	$domain = $_SERVER['HTTP_HOST'];
+	if ( !in_array($domain, $arr) ){
+	    $deny = explode(',',C('OTHERS'));
+	    $this->redirect('', $deny[array_rand($deny)]);
+	}
 	$this->set('domain', $_SERVER['HTTP_HOST']);
 	$this->display();
     }
--- web/Config/public.config.php
+++ web/Config/public.config.php
@@ -15,7 +15,10 @@ $define = array(
     'OPENAPI_USERS' => array(
         'TmSystem' => 'api1001',//商标系统
     ),
+    'ALLOW' => 'doulaisong.com,xishanpo.com,songxiaoer.cc,songxiaoer.net,eastward.xyz,douob.xyz,douob.cc,douob.cn,eastward.cc,folin.cc,chaofan.bid,10s.bid,yizhichan.xyz,bind.red,continue.red,finally.red,function.red,exit.red,static.red,rand.red,tingcc.com,biaoyuan.org,chabiao.org,637000.ren,xxooxo.com,xxooxo.net,nigeda.com,lovexxoo.cn,baoipr.com,zhizhichan.com,fanpay.cn,xxooxo.cn,xuni.name,yiizc.cn,yiizc.com,yiipo.cn,yiipr.com,godlove.top,godlove.ren,ourself.xyz,ourself.top,saler.xyz,ichaofan.top,ichaofan.xyz,xxooxo.top,idou.ren,zhangxu.space,zhangxu.site,aixxoo.win,aixxoo.club,aixxoo.site,aixxoo.xyz,aixxoo.ren,ixxoo.win,ixxoo.club,ixxoo.site,ixxoo.xyz,ixxoo.ren,chaofan.space,chaofan.trade,xxooxo.trade,xxooxo.win,xxooxo.club,xxooxo.ren,xxooxo.site,xxooxo.xyz,tito.ren,ichaofan.wang,buyii.cn,payii.cn,fisher.ren,buyer.ren,godloveit.com,goduseit.com,godloveme.net,lovexxoo.ren,zhangxu.ren,aixxoo.wang,ixxoo.wang,lovexxoo.wang,chaofan.link,zhangxu.link,637000.club,zhangxu.xyz',
     
+    'OTHERS' => 'http://www.crazyenglish.com/,http://www.xuexifangfa.com/,http://www.zz6789.com/,http://sw.zxxk.com/,http://huaxue.zxxk.com/,http://wl.zxxk.com/,http://www.52en.com/,http://www.12999.com/,http://sdz.cn/srx/,http://www.5156edu.com/,http://www.czsx.com.cn/,http://www.zhyww.cn,http://www.zhoujielu.com/,http://www.bcw989.com/,http://www.sis001.com/,http://www.sexinsex.net/,http://www.bet356.net/',
+
 );
 
 
