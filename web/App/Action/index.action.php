d3def78e138af330a9449d9e78903b48aa56b733
eastwards <xtvj007@gmail.com>
2016-06-22 18:55:04 +0800

    tongji

--- web/App/Action/index.action.php
+++ web/App/Action/index.action.php
@@ -10,7 +10,7 @@
  */
 class IndexAction extends AppAction
 {
-    public $caches  	= array('index');
+    //public $caches  	= array('index');
     public $cacheId 	= 'redisHtml';
     public $expire  	= 36000;//1小时
 
@@ -24,10 +24,26 @@ class IndexAction extends AppAction
 	if ( !in_array($domain, $arr) ){
 	    $deny = explode(',',C('OTHERS'));
 	    $this->redirect('', $deny[array_rand($deny)]);
+	}else{
+	    $num = intval($this->com('redisHtml')->get($domain.date('_Ymd')));
+	    $this->com('redisHtml')->set($domain.date('_Ymd'), ++$num);
 	}
 	$this->set('domain', $_SERVER['HTTP_HOST']);
 	$this->display();
     }
 
+    public function clear()
+    {
+	$cid = $this->input('cid', 'int', -1);
+	//redis默认15个数据库，如redis配置文件修改，这里也要修改
+	if ( $cid < 0 || !in_array($cid, range(0,14)) ){
+	    $this->returnAjax(array('code'=>2,'msg'=>'参数错误'));
+	}
+
+	//清除缓存（注意：清除前要关闭缓存配置，否则不会执行到这里）
+	$this->com('qcache')->select($cid)->clear();
+	$this->returnAjax(array('code'=>1));
+    }
+	
 }
 ?>
